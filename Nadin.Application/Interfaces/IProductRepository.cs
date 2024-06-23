1ba5edeb859422fd0d9c309f85d9a2872bfc8320
MsN <mohsen.s7s.ab@gmail.com>
2024-06-23 12:27:29 +0330

    add product interfaces and services

---
+++ Nadin.Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+using Nadin.Core.Entities;
+
+namespace Nadin.Application.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<Product> GetByIdAsync(int id);
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(Product product);
+    }
+}
---
+++ Nadin.Application/Services/ProductRepository.cs
@@ -0,0 +1,45 @@
+using Nadin.Core.Entities;
+using Nadin.Application.Interfaces;
+using Nadin.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Nadin.Application.Services
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ProductRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product> GetByIdAsync(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task AddAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Product product)
+        {
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
